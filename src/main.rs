ee434205fd9c9a4eb720cb471ffb70955918abca
Trond H Emaus <trondhe@gmail.com>
2020-03-17 23:04:10 +0100

    move pixel into its own mod

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,9 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-struct Pixel {
-    r: u32,
-    g: u32,
-    b: u32,
-}
+mod pixel;
 
-impl Pixel {
-    #[allow(dead_code)]
-    fn new() -> Self {
-        Pixel { r: 0, g: 0, b: 0 }
-    }
-}
-
-impl IntoIterator for Pixel {
-    type Item = u32;
-    type IntoIter = PixelIntoIterator;
-
-    fn into_iter(self) -> Self::IntoIter {
-        PixelIntoIterator {
-            pixel: self,
-            index: 0,
-        }
-    }
-}
-
-struct PixelIntoIterator {
-    pixel: Pixel,
-    index: usize,
-}
-
-impl Iterator for PixelIntoIterator {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<u32> {
-        let result = match self.index {
-            0 => self.pixel.r,
-            1 => self.pixel.g,
-            2 => self.pixel.b,
-            _ => return None,
-        };
-        self.index += 1;
-        Some(result)
-    }
-}
-
-impl<'a> IntoIterator for &'a Pixel {
-    type Item = u32;
-    type IntoIter = PixelIterator<'a>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        PixelIterator {
-            pixel: self,
-            index: 0,
-        }
-    }
-}
-
-struct PixelIterator<'a> {
-    pixel: &'a Pixel,
-    index: usize,
-}
-
-impl<'a> Iterator for PixelIterator<'a> {
-    type Item = u32;
-    fn next(&mut self) -> Option<u32> {
-        let result = match self.index {
-            0 => self.pixel.r,
-            1 => self.pixel.g,
-            2 => self.pixel.b,
-            _ => return None,
-        };
-        self.index += 1;
-        Some(result)
-    }
-}
+use pixel::Pixel;
 
 fn main() {
     let data: Vec<Vec<Pixel>> = vec![vec![
---
+++ src/pixel.rs
@@ -0,0 +1,75 @@
+pub struct Pixel {
+    pub r: u32,
+    pub g: u32,
+    pub b: u32,
+}
+
+impl Pixel {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        Pixel { r: 0, g: 0, b: 0 }
+    }
+}
+
+impl IntoIterator for Pixel {
+    type Item = u32;
+    type IntoIter = PixelIntoIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        PixelIntoIterator {
+            pixel: self,
+            index: 0,
+        }
+    }
+}
+
+pub struct PixelIntoIterator {
+    pixel: Pixel,
+    index: usize,
+}
+
+impl Iterator for PixelIntoIterator {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        let result = match self.index {
+            0 => self.pixel.r,
+            1 => self.pixel.g,
+            2 => self.pixel.b,
+            _ => return None,
+        };
+        self.index += 1;
+        Some(result)
+    }
+}
+
+impl<'a> IntoIterator for &'a Pixel {
+    type Item = u32;
+    type IntoIter = PixelIterator<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        PixelIterator {
+            pixel: self,
+            index: 0,
+        }
+    }
+}
+
+pub struct PixelIterator<'a> {
+    pixel: &'a Pixel,
+    index: usize,
+}
+
+impl<'a> Iterator for PixelIterator<'a> {
+    type Item = u32;
+    fn next(&mut self) -> Option<u32> {
+        let result = match self.index {
+            0 => self.pixel.r,
+            1 => self.pixel.g,
+            2 => self.pixel.b,
+            _ => return None,
+        };
+        self.index += 1;
+        Some(result)
+    }
+}
